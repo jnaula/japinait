30612bb3d8e360164464f63522762fa42ce60023
jnaula <jnaula58@gmail.com>
2026-03-17 13:51:19 -0500

    editar localesfin

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ export default function App() {
               } 
             />
             <Route
-              path="/venue/:id/edit"
+              path="/edit-venue/:id"
               element={
                 <ProtectedRoute>
                   <EditVenue />
