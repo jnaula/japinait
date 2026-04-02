3a4fa914061ced15d31af874f85e79ae34a30c15
jnaula <jnaula58@gmail.com>
2026-04-02 14:02:12 -0500

    fixrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 import { APIProvider } from '@vis.gl/react-google-maps'; // ✅ Agregar este import
 import Navigation from './components/Navigation';
